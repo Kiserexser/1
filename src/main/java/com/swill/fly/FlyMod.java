1d5ca87287fad2f4b235fde821c8f520ee30fb29
Kiserexser <towartors@mail.ru>
2026-05-06 17:22:53 +0300

    Create FlyMod.java

---
+++ src/main/java/com/swill/fly/FlyMod.java
@@ -0,0 +1,16 @@
+{
+  "schemaVersion": 1,
+  "id": "flymod",
+  "version": "1.0.0",
+  "name": "FlyMod",
+  "description": "Fly on G",
+  "authors": ["Swill"],
+  "entrypoints": {
+    "main": ["com.swill.fly.FlyMod"]
+  },
+  "depends": {
+    "fabricloader": ">=0.16.9",
+    "minecraft": "1.21.4",
+    "fabric-api": "*"
+  }
+}
